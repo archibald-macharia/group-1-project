270e7018cee4932b9ffa9925f8eea11886d5996b
Peris Wakio <peris.kingori@student.moringaschool.com>
2023-09-12 13:31:12 -0700

    Home.js

--- nisaidie/src/App.js
+++ nisaidie/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./home"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <Home />
     </div>
   );
 }
