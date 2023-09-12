70b54c86b71f05e95e7e7a668fde0c4c9f85cc14
Archibald Macharia Gacheru <archibald.gacheru@student.moringaschool.com>
2023-09-12 21:59:15 +0300

    Create the homepage

--- nisaidie/src/home.js
+++ nisaidie/src/home.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Home(){
     return(
-        <div>main</div>
+        <div id='header'>
+            <div id='logo'>
+                Ns.
+            </div>
+            <div className='Navbar'>
+                <ul>
+                    <li><a href='#home'>Home</a></li>
+                    <li><a href='#counselor'>Counselor</a></li>
+                    <li><a href='#inspiration'>Stories</a></li>
+                    <li><a href='#about'>About Us</a></li>
+                    <li><a href='#Contact'>Contact</a></li>
+                    <li><a href='#sign-up'>Sign Up</a></li>
+
+                </ul>
+            </div>
+        </div>
     );
 }
 
