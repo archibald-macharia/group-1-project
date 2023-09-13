a7c894bb4f565a69c326d3f1db6a3277894bfd0a
Abby-kr <abby.a.otieno@gmail.com>
2023-09-13 15:10:26 +0300

    Added form

--- nisaidie/src/NewStoryForm.js
+++ nisaidie/src/NewStoryForm.js
@@ -8,7 +8,15 @@ function newStoryForm({trigger, setTrigger, onAddStory}) {
         <div className='pop-up-container'>
           <div className='pop-up-content'>
             <button className='pop-up-btn' onClick={() => setTrigger(false)}>X</button>
-            
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+
+            <label htmlFor="message">Story:</label>
+            <textarea id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)}/>
+
+            <button type="submit">Submit</button> 
+            </form>
           </div>           
         </div>
     ) : ""
