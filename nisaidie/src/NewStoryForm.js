3028f40075457e6c0d91b4e7f2aacddc2ec2f45f
Abby-kr <abby.a.otieno@gmail.com>
2023-09-13 15:09:31 +0300

    renamed file and added popup code

--- nisaidie/src/NewStoryForm.js
+++ nisaidie/src/NewStoryForm.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function newStoryForm(props) {
-    return ()
+function newStoryForm({trigger, setTrigger, onAddStory}) {
+    //const [formData,setFormData] = useState({})
+  
+
+    return (trigger) ? (
+        <div className='pop-up-container'>
+          <div className='pop-up-content'>
+            <button className='pop-up-btn' onClick={() => setTrigger(false)}>X</button>
+            
+          </div>           
+        </div>
+    ) : ""
 }
 
 export default newStoryForm;
