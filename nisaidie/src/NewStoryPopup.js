68ab12fc29ea4f14747473369e1ac589fecc1999
Abby-kr <abby.a.otieno@gmail.com>
2023-09-13 22:16:31 +0300

    Updated file

--- nisaidie/src/NewStoryPopup.js
+++ nisaidie/src/NewStoryPopup.js
@@ -1,42 +1,54 @@
 import React, {useState} from 'react';
 
-function newStoryForm({trigger, setTrigger, onAddStory}) {
-    const [name,setName] = useState("")
+function NewStoryPopup({trigger, setTrigger, onAddStory}) {
+    const [firstName,setFirstName] = useState("")
+    const [lastName,setLastName] = useState("")
     const [message,setMessage] = useState("")
 
     function handleSubmit(event){
         event.preventDefault()
+
         const formData = {
-            Name: name,
-            Story: message
+            Firstname: firstName,
+            Lastname: lastName,
+            Story: message,
         }
+
         fetch("http://localhost:3000/userStories",{
             method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
         })
-        .then((r) => r.json)
+        .then((r) => r.json())
         .then((newStory) => onAddStory(newStory))
+
+        setFirstName("")
+        setLastName("")
+        setMessage("")
     }
 
     return (trigger) ? (
         <div className='pop-up-container'>
           <div className='pop-up-content'>
             <button className='pop-up-btn' onClick={() => setTrigger(false)}>X</button>
+            <h5 className='form-title'>Add YourStory</h5>
             <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+            <label >Firstname:</label>
+            <input type="text" placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+            
+            <label >Lastname:</label>
+            <input type="text" placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)}/>
 
-            <label htmlFor="message">Story:</label>
-            <textarea id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)}/>
+            <label >Story:</label>
+            <textarea id="message" value={message} onChange={(e) => setMessage(e.target.value)}/>
 
-            <button type="submit">Submit</button> 
+            <button type="submit" className='submit-btn'>Submit</button> 
             </form>
           </div>           
         </div>
     ) : ""
 }
 
-export default newStoryForm;
+export default NewStoryPopup;
