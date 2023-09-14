ad474961bfe5bcdeab125c1103a91698c5d76808
Abby-kr <136746644+Abby-kr@users.noreply.github.com>
2023-09-15 01:33:53 +0300

    Added DELETE request and click handler

--- nisaidie/src/Story.js
+++ nisaidie/src/Story.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-function Story({firstName, lastName ,userStory}) {
+function Story({firstName, lastName ,userStory, onDeleteCard, storyId}) {
 
+    function removeCard(){
+        fetch(`http://localhost:3000/userStories/${storyId}`,{
+            method: "DELETE",
+        })
+        .then((r) => r.json())
+        .then(() => onDeleteCard(userStory))
+    }
+    
     return (
         <div className='card-container'>
         <div className='card-content'>
+                <button className='delete-btn' onClick={() => removeCard(storyId)}>X</button>
                 <h3 className='card-name'>{firstName} {lastName}</h3>
                 <p className='card-body'>{userStory}</p>
             </div>
