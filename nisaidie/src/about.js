72ad9d627b25cbe4db686d15a832247011104cff
Abby-kr <abby.a.otieno@gmail.com>
2023-09-14 22:38:50 +0300

    Added code for about cards

--- nisaidie/src/about.js
+++ nisaidie/src/about.js
@@ -2,7 +2,38 @@ import React from 'react'
 
 function About(){
     return(
-        <div>main</div>
+        <div>
+            <div className='about-cards'>
+            <div className='individual-card'>
+                <img src='https://st3.depositphotos.com/17828278/33150/v/450/depositphotos_331503262-stock-illustration-no-image-vector-symbol-missing.jpg'
+                className='about-img'
+                alt='Developer 1'></img>
+                <h3>Abigail Otieno</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+            </div>
+            <div className='individual-card'>
+                <img src='https://st3.depositphotos.com/17828278/33150/v/450/depositphotos_331503262-stock-illustration-no-image-vector-symbol-missing.jpg'
+                className='about-img'
+                alt='Developer 2'></img>
+                <h3>Archibal Gacheru</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+            </div>
+            <div className='individual-card'>
+                <img src='https://st3.depositphotos.com/17828278/33150/v/450/depositphotos_331503262-stock-illustration-no-image-vector-symbol-missing.jpg'
+                className='about-img'
+                alt='Developer 3'></img>
+                <h3>Peris Kingori</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+            </div>
+            <div className='individual-card'>
+                <img src='https://st3.depositphotos.com/17828278/33150/v/450/depositphotos_331503262-stock-illustration-no-image-vector-symbol-missing.jpg'
+                className='about-img'
+                alt='Developer 4'></img>
+                <h3>Newton Bundi</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+            </div>
+            </div>
+        </div>
     );
 }
 
