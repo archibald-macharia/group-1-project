d8cf444074f6d8415d51d53059373cc787cd1c8a
Abby-kr <abby.a.otieno@gmail.com>
2023-09-14 23:15:59 +0300

    Updated file

--- nisaidie/src/StoriesList.js
+++ nisaidie/src/StoriesList.js
@@ -1,9 +1,7 @@
 import React,{useState} from 'react'
-import { userStories } from './data';
 import Story from './Story';
 
 function StoriesList(){
-    const [stories,setStories] = useState(userStories)
     return(
         <div className="story-lists">
             {stories.map((story) => 
