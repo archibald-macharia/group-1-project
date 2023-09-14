a204d3d6e9c44d0262961bf1999c270bb51fd1f3
Abby-kr <abby.a.otieno@gmail.com>
2023-09-14 22:38:02 +0300

    Imported about

--- nisaidie/src/App.js
+++ nisaidie/src/App.js
@@ -1,10 +1,12 @@
 import StoriesList from './StoriesList';
+import About from './About';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <StoriesList />
+      <About />
     </div>
   );
 }
