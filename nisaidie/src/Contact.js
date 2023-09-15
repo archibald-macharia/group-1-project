9b5e42b38047d9c03e2c6ed37a81d2c95d89db77
Abby-kr <136746644+Abby-kr@users.noreply.github.com>
2023-09-15 23:01:07 +0300

    Added contact code

--- nisaidie/src/Contact.js
+++ nisaidie/src/Contact.js
@@ -1,1 +1,21 @@
+import React from 'react';
 
+function Contact() {
+    return (
+        <div className='contact-page'>
+            <h1>Contact Us</h1>
+            <p>Have any inquiries, suggestions, concerns, or feedback?</p>
+            <p>We'd love to hear from you.</p>
+            <div className='contacts'>
+                <h4>Emails</h4>
+                <p>Abigail Otieno : abigailotieno@gmail.com</p>
+                <p>Peris Kingori :periskingori@gmail.com</p>
+                <p>Archibald Gacheru : archibaldgacheru@gmail.com</p>
+                <p>Newton Bundi : newtonBundi@gmail.com</p>
+            </div>
+            <p className='contact-paragraph'>We appreciate any message you have for us!</p>
+        </div>
+    );
+}
+
+export default Contact;
